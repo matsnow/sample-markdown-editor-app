6f6821a675d2d3d4fd09e2c8e87343eef5070cbd
Masayuki Matsuo <masayuki.matsuo@benesse-i-career.co.jp>
2017-11-14 19:16:56 +0900

    eslintに.vue用のプラグインを追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,19 @@
 module.exports = {
-    "extends": "google"
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/recommended' // or 'plugin:vue/base'
+  ],
+  rules: {
+    // override/add rules' settings here
+    'vue/valid-v-if': 'error',
+    "rules": {
+      "semi": ["error", "always"],
+      "arrow-body-style": "error",
+      "no-duplicate-imports": "error",
+      "no-var": "error",
+    }
+  },
+  env: {
+    node: true
+  }
 };
