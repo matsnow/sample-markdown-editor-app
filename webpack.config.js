3c03456cc00207bcab3dc1bf4bd0b1ab33b665b4
Masayuki Matsuo <masayuki.matsuo@benesse-i-career.co.jp>
2017-11-14 22:51:38 +0900

    パッケージ生成用のコマンドを追加

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
-const path = require('path');                                                                                                                          
+const path = require('path');
+
 module.exports = { 
   context: path.resolve(__dirname, './src'),
   entry: './app.js',
